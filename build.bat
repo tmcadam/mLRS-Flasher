aad2bcd62fdb706beff204ed7f0efd374693ed73
Tom McAdam <mcadam1980@gmail.com>
2025-12-10 22:54:49 +0900

    Add build.bat for windows

---
+++ build.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+echo Building executable with PyInstaller...
+pyinstaller mLRS_Flasher.py ^
+    --noconsole ^
+    --hidden-import=pymavlink ^
+    --hidden-import=pymavlink.mavutil ^
+    --hidden-import=pymavlink.dialects.v20.common ^
+    --add-data "venv\Lib\site-packages\pymavlink\dialects;pymavlink/dialects" ^
+    --add-data "venv\Lib\site-packages\esptool\targets\stub_flasher;esptool/targets/stub_flasher" ^
+    --add-data "assets\*;assets" ^
+    --add-data "thirdparty\stm32cubeprogrammer\win*;stm32cubeprogrammer" ^
+    --icon "assets\mLRS_logo_round.ico" ^
+    --name "mLRS_Flasher"
+
+
+echo.
+echo Build complete. EXE should be in the dist/ folder.
+pause
